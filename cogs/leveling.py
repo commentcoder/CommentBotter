700a196296b36d331684a877d9e0e966b07791e7
Thomas Collart <thomas.collart@gmail.com>
2025-01-18 09:57:03 +0700

    Add clickable link in embed

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -101,7 +101,7 @@ class Leveling(commands.Cog):
 
         embed.add_field(name="⭐ Noter un de mes cours Udemy", value="Donner une note à une de mes formations payantes Udemy : **+100 XP**", inline=False)
         embed.add_field(name="📩 Inviter un nouveau membre", value="Invitez vos amis à rejoindre le serveur : **+30 XP**", inline=False)
-        embed.add_field(name="🤝 Aider quelqu'un dans le forum #🆘┃forum-aide", value="Donnez une réponse utile ou résolvez un problème : **+30 XP**", inline=False)
+        embed.add_field(name="🤝 Aider quelqu'un dans [le forum d'aide])(https://discord.com/channels/1115999077776240682/1307900991563108382)", value="Donnez une réponse utile ou résolvez un problème : **+30 XP**", inline=False)
         embed.add_field(name="💬 Poster un message", value="Postez un message (1 fois par minute pour éviter le flood) : **+1 XP**", inline=False)
         embed.add_field(name="🐞 Créer une issue sur GitHub", value="Signalez un bug ou une amélioration pertinente : **+10 XP**", inline=False)
         embed.add_field(name="🔄 Faire une Pull Request sur GitHub", value="Contribuez à nos projets open-source : **+20 XP**", inline=False)
